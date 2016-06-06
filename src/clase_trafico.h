cbaaecfe9608c7218b96505790a09c1c1345e8d6
Yonatan Romero <yromero@openmailbox.org>
2016-06-06 12:03:26 -0300

    Refactor + test

--- src/clase_trafico.h
+++ src/clase_trafico.h
@@ -23,9 +23,11 @@
  * dirección de red.
  */
 struct s_subnet {
-    in_addr red; /* Direccion de red (la seccion de host debe estar en cero) */
+    struct in_addr red; /* Direccion de red (la seccion de host debe estar 
+                         * en cero) 
+                         */
     u_int32_t mascara; /* Mascara de subred en formato hexadecimal */
-}
+};
 
 /**
  * t_clase
@@ -50,22 +52,10 @@ typedef struct s_clase {
     int cant_subredes;  /* Cantidad de subredes que tiene la clase */
     int cant_puertos;   /* Cantidad de puertos que tiene la clase */
     int protocolo;      /* Protocolo que aplica esta clase */
-    s_subnet *subredes; /* Array de subredes que definen la clase */
+    struct s_subnet *subredes; /* Array de subredes que definen la clase */
     int *puertos;       /* Array de puertos que definen la clase */
-    s_clase *siguiente; /* Estructura siguiente en la lista */
+    struct s_clase *siguiente; /* Estructura siguiente en la lista */
 } t_clase;
-
-/**
- * IN_NET(a, n, m)
- * --------------------------------------------------------------------------
- * Devuelve verdadero si la direccion *a* pertenece a la red *n* con la mascara
- * de subred *m*
- *
- * Para comprobar que una direccion IP pertenece a una IP hay que hacer una
- * operación AND con la máscara de subred y el resultado debe ser igual a la
- * dirección de red.
- */
-#define IN_NET(a, n, m) ((in_addr) (a & m) == n)
 
 /**
  * MASCARA(n)
@@ -78,5 +68,18 @@ typedef struct s_clase {
  * es 0xfffff800
  */
 #define MASCARA(n) 0xffffffff & ~(0xffffffff >> n)
+
+/**
+ * in_net
+ * --------------------------------------------------------------------------
+ * Devuelve TRUE si la direccion *ip* pertenece a la red *red* con la mascara
+ * de subred *mascara*. La mascara de subred debe estar en formato hexadecimal
+ *
+ * Para comprobar que una direccion IP pertenece a una IP hay que hacer una
+ * operación AND con la máscara de subred y el resultado debe ser igual a la
+ * dirección de red.
+ */
+int in_net(struct inet_addr ip, struct inet_addr red, u_int32_t mascara);
+
 
 #endif /* CLASE_TRAFICO_H */
---
+++ tests/test_clase_trafico.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include "clase_trafico.h"
+
+#define MASCARA_0  0x0
+#define MASCARA_8  0xff000000
+#define MASCARA_16 0xffff0000
+#define MASCARA_21 0xfffff800
+#define MASCARA_24 0xffffff00
+#define MASCARA_27 0xffffffe0
+#define MASCARA_31 0xfffffffe
+
+
+void test_MASCARA(void) {
+    assert(MASCARA(0) == MASCARA_0);
+    assert(MASCARA(8) == MASCARA_8);
+    assert(MASCARA(16) == MASCARA_16);
+    assert(MASCARA(21) == MASCARA_21);
+    assert(MASCARA(24) == MASCARA_24);
+    assert(MASCARA(27) == MASCARA_27);
+    assert(MASCARA(31) == MASCARA_31);
+}
+
+int main() {
+    test_MASCARA();
+    return 0;
+}
